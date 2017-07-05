06527959840cfb2e009bcbb61bde8a59ca89c3b0
tyler-hilbert <tylerjoshuahilbert@gmail.com>
2017-07-05 15:19:02 -0400

    added test example and test benches for multiplication.. It looks like there is a problem with the carry in multiplication

--- Multiplication/MultiplierTest.v
+++ Multiplication/MultiplierTest.v
@@ -1,17 +1,18 @@
 module Multiplier_Test;
-	wire c0, c1, c2, c3;
-	reg a0=0, a1=0, b0=0, b1=0;
+	wire c0, c1, c2, c3; // Outputs
+	wire a0, a1, b0, b1; // Inputs
+	integer i;
 
 	initial begin
-	    $dumpfile("test.vcd");
-     	$dumpvars(0, Multiplier_Test);
-     	
-     	# 90 $stop;
-  	end
+    	$dumpfile("test.vcd");
+    	$dumpvars(0, Multiplier_Test);
+    	for (i = 0; i < 16; i=i+1)
+        	#10;
+    	$stop;
+	end
+
+	assign a0 = i[0], a1 = i[1], b0 = i[2], b1 = i[3];
+
 
 	Multiplier mult (a0, a1, b0, b1, c0, c1, c2, c3);
-
-	initial begin
-     	//$monitor("a: %h, b: %h, sum: %h carry: %h", a, b, sum, carry);
-    end
 endmodule
