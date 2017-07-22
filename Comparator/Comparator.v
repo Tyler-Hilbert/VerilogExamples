6d836f2b746b91a4726b2a496a460d7cb12a3d62
tyler-hilbert <tylerjoshuahilbert@gmail.com>
2017-07-22 14:15:45 -0400

    Comparator using operators is working

--- Comparator/Comparator.v
+++ Comparator/Comparator.v
@@ -1,8 +1,26 @@
-module Comparator(a[3:0], b[3:0], aHigher, bHigher, equal);
+module ComparatorWithOperators(a[3:0], b[3:0], aHigher, bHigher, equal);
 	input [3:0] a, b;
 	output aHigher, bHigher, equal;
 
-	assign aHigher = a[0];
-	assign bHigher = b[0];
-	assign equal = b[1];
+   	assign aHigher = a > b;
+   	assign bHigher = a < b;
+   	assign equal = a~|b;
+
+endmodule
+
+module ComparatorWithControlStatements(a[3:0], b[3:0], aHigher, bHigher, equal);
+	input [3:0] a, b;
+	output aHigher, bHigher, equal;
+	//assign equal = 0;
+
+	//integer i;
+
+	//always@(*)
+	//begin
+	//for(i = 0; i <= 3; i++)
+		//if (equal == 0)
+			//assign bHigher = !a[0]&b[0];
+			//assign aHigher = !b[0]&a[0];
+			//assign equal = aHigher~|bHigher;
+   	//end
 endmodule
--- Comparator/ComparatorTest.v
+++ Comparator/ComparatorTest.v
@@ -1,11 +1,19 @@
 module ComparatorTest;
-	wire [3:0] a = 15;
-	wire [3:0] b = 0;
+	reg [3:0] a;
+	reg [3:0] b;
+	output aHigher, bHigher, equal;
+
+	ComparatorWithOperators c (a, b, aHigher, bHigher, equal);
 
 	initial begin
 	    $dumpfile("test.vcd");
     	$dumpvars(0, ComparatorTest);
-		#10;
-		#20 $stop;
+
+		# 0  a[3:0]=4'b0000; b[3:0]=4'b0000;
+		# 20 a[3:0]=4'b1111; b[3:0]=4'b1111;
+		# 40 a[3:0]=4'b0000; b[3:0]=4'b1111;
+		# 60 a[3:0]=4'b1111; b[3:0]=4'b0000;
+		# 80 a[3:0]=4'b1000; b[3:0]=4'b0111;
+		# 100 $stop;
 	end
 endmodule
